f22e89ba0a7cf6f04262c8205257592085cbfb9d
ShieldyArmor <Thegreen_dragon@hotmail.com>
2023-03-07 10:17:48 +0100

    Added React router dom and basic routing

---
+++ project-blackbird/src/Home.js
@@ -0,0 +1,14 @@
+// import useFetch from "./useFetch";
+
+const Home = () => {
+  // const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
+  return (
+    <div className="home">
+    <h2>Homepage</h2>
+    <p>This is the temporary homepage while Arijus works on the design.</p>
+    </div>
+  );
+}
+ 
+export default Home;
