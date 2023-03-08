e115fe74efb902a935d69717b44824c6181e1c54
AriLaurin <arijuslaurin@gmail.com>
2023-03-08 15:10:47 +0100

    editing header

--- project-blackbird/src/Home.js
+++ project-blackbird/src/Home.js
@@ -1,13 +1,27 @@
 // import useFetch from "./useFetch";
 import "./home.css";
+import logo from "./img/BBPlogo.png";
 
 const Home = () => {
   // const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
   return (
     <div className="home">
-    <h2>Homepage</h2>
-    <p>This is the temporary homepage while Arijus works on the design.</p>
+      <div className="container">
+          
+    <div class="header-left">
+          <img id="logo" src={logo} alt="Logo" width="91px" height="100px"/>
+          <h1 id="logotext">BLACKBIRD PROJECT</h1>
+    </div>
+    <div class="header-right">
+          <h2 className="headertabs">ABOUT US</h2>
+          <h2 className="headertabs">PROJECTS</h2>
+    </div>
+        <div className="body">
+        </div>
+        <div className="footer"></div>
+      </div>
+      {/* End of Container ^ */}
     </div>
   );
 }
